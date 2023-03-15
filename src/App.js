c3302a892e819e7cbd9d48ad61ac8a69bdbbc5ea
Wes <wfreemansmith@yahoo.co.uk>
2023-03-15 11:02:22 +0000

    topic select from each screen

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import FrontPage from "./components/FrontPage";
 import Article from "./components/Article";
+import { getTopics } from "./utils/api";
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [topicList, setTopicList] = useState([{ slug: "", description: "Frontpage headlines" }]);
+  const [topicList, setTopicList] = useState([]);
+  const [description, setDescription] = useState("");
   const [user, setUser] = useState({
     username: "tickle122",
     name: "Tom Tickle",
@@ -15,15 +17,45 @@ function App() {
       "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
   });
 
+  useEffect(() => {
+    getTopics().then(({ topics }) => {
+      setTopicList([...topics]);
+    });
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <Navigation setTopicList={setTopicList} topicList={topicList} />
+      <Navigation
+        setTopicList={setTopicList}
+        topicList={topicList}
+        setDescription={setDescription}
+        description={description}
+      />
       <Routes>
-        <Route path="/" element={<FrontPage />} />
-        <Route path="/articles/:article_id" element={<Article user={user} />} />
+        <Route
+          path="/"
+          element={
+            <FrontPage setDescription={setDescription} />
+          }
+        />
+        <Route
+          path="/articles/:article_id"
+          element={
+            <Article
+              user={user}
+              topicList={topicList}
+              setDescription={setDescription}
+            />
+          }
+        />
         <Route path="/users/:username" element={<FrontPage />} />
-        <Route path="/:topic" element={<FrontPage topicList={topicList} />} />
+        <Route
+          path="/:topic"
+          element={
+            <FrontPage topicList={topicList} setDescription={setDescription} />
+          }
+        />
       </Routes>
     </div>
   );
