5bc18dada5bdacae94f516702dbf3725153dc278
Wes <wfreemansmith@yahoo.co.uk>
2023-03-15 17:03:25 +0000

    delete button module added

---
+++ src/components/CommentDelete.jsx
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+import { deleteComment } from "../utils/api";
+
+function CommentDelete({thisComment, comments, setComments}) {
+  const [deleting, setDeleting] = useState("working");
+
+  function handleDelete(comment_id) {
+    setDeleting("in-progress")
+    deleteComment(comment_id)
+      .then(() => {
+        setComments(comments.filter((comment) => comment !== thisComment))
+      })
+      .catch(() => {
+        setDeleting("error");
+      });
+  }
+
+  return (
+    (
+      <button
+        className={deleting}
+        onClick={() => {
+          handleDelete(thisComment.comment_id);
+        }}
+      >
+      {deleting === "in-progress" ? "Deleting..." : "Delete"}
+      </button>
+    )
+  )
+}
+
+export default CommentDelete
