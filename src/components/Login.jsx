7f2b5f7173ac02845a9d115ba0f59e407a488f11
Wes <wfreemansmith@yahoo.co.uk>
2023-04-28 06:31:46 +0100

    login get request added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,14 @@
 import { BsSunFill, BsSun } from "react-icons/bs";
-import { FaUserCircle, FaRegUserCircle} from "react-icons/fa"
+import { FaUserCircle, FaRegUserCircle } from "react-icons/fa";
 import { useContext } from "react";
 import { ThemeContext } from "../contexts/Theme";
+import { UserContext } from "../contexts/User";
 
-function Login({setLogin}) {
+function Login({ setLogin }) {
   const { theme, setTheme } = useContext(ThemeContext);
+  const { user } = useContext(UserContext);
+
+  const isUser = Object.keys(user).length
 
   const toggleDarkMode = () => {
     theme === "" ? setTheme("dark") : setTheme("");
@@ -12,24 +16,25 @@ function Login({setLogin}) {
 
   return (
     <>
-    <div className="login">
-      <button
-        className={"dark-button function-button " + theme}
-        onClick={() => {
-          setLogin(true);
-        }}
-      ><FaUserCircle/>
-      </button>
+      <div className="login">
+        <button
+          className={"dark-button function-button " + theme}
+          onClick={() => {
+            setLogin(true);
+          }}
+        >
+          {isUser ? <FaUserCircle /> : <FaRegUserCircle/>}
+        </button>
       </div>
       <div className="light-toggle">
-      <button
-        className={"dark-button function-button " + theme}
-        onClick={() => {
-          toggleDarkMode();
-        }}
-      >
-        {theme === "" ? <BsSunFill /> : <BsSun />}
-      </button>
+        <button
+          className={"dark-button function-button " + theme}
+          onClick={() => {
+            toggleDarkMode();
+          }}
+        >
+          {theme === "dark" ? <BsSunFill /> : <BsSun />}
+        </button>
       </div>
     </>
   );
